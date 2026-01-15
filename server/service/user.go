12e9d3ffbb4aeda16621b1e585373a39eba60b3f
AAAkater <aaakater@qq.com>
2026-01-15 22:39:11 +0800

    ✨ feat(user): implement user registration service
    - add user registration method with username validation
    - hash user password using bcrypt before storage
    - check for duplicate usernames in database
    - return appropriate error messages for registration failures

--- server/service/user.go
+++ server/service/user.go
@@ -1,9 +1,28 @@
 package service
 
+import (
+	"errors"
+	"server/db"
+	"server/models"
+	"server/utils"
+
+	"gorm.io/gorm"
+)
+
 var UserServiceApp = new(UserService)
 
 type UserService struct{}
 
-func (this *UserService) AddNewUser() {
+func (this *UserService) Register(new_user models.User) error {
+	var user models.User
+	res := db.PgSqlDB.Where("username = ?", new_user.Username).First(&user)
 
+	if res.Error != gorm.ErrRecordNotFound {
+		return errors.New("this username has been already used")
+	}
+
+	new_user.Password = utils.BcryptHash(new_user.Password)
+	err := db.PgSqlDB.Create(&new_user).Error
+
+	return err
 }

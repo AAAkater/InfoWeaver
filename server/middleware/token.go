fcab5d8cc50f99010119895bc424ad656abccff6
AAAkater <aaakater@qq.com>
2026-01-23 20:48:02 +0800

    ✨ feat(middleware): add JWT token middleware
    - implement TokenMiddleware function using echo-jwt
    - configure JWT signing key from application settings
    - set up custom claims handling with JwtCustomClaims

--- server/middleware/token.go
+++ server/middleware/token.go
@@ -1,1 +1,20 @@
 package middleware
+
+import (
+	"server/config"
+	"server/utils"
+
+	"github.com/golang-jwt/jwt/v5"
+	echoJwt "github.com/labstack/echo-jwt/v5"
+	"github.com/labstack/echo/v5"
+)
+
+func TokenMiddleware() echo.MiddlewareFunc {
+	config := echoJwt.Config{
+		NewClaimsFunc: func(c *echo.Context) jwt.Claims {
+			return new(utils.JwtCustomClaims)
+		},
+		SigningKey: config.Settings.GetJWTSigningKey(),
+	}
+	return echoJwt.WithConfig(config)
+}

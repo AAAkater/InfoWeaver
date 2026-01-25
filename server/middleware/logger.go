e6855890591d7901281e3bcf218df36e48ec0a42
AAAkater <aaakater@qq.com>
2026-01-25 19:54:30 +0800

    ♻️ refactor(logger): update echo version and simplify log format
    - upgrade from echo/v4 to echo/v5 middleware
    - change handler signature to use pointer to echo.Context
    - remove http status code from log output for cleaner format

--- server/middleware/logger.go
+++ server/middleware/logger.go
@@ -4,24 +4,23 @@ import (
 	"server/utils"
 	"time"
 
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v5"
 )
 
 func LoggerMiddleware() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(ctx echo.Context) error {
+		return func(ctx *echo.Context) error {
 			start := time.Now()
 
 			err := next(ctx)
 
 			duration := time.Since(start)
 			req := ctx.Request()
-			resp := ctx.Response()
-			utils.Logger.Infof("%s uri=%s ip=%s status=%d duration=%v error=%v",
+
+			utils.Logger.Infof("%s uri=%s ip=%s duration=%v error=%v",
 				req.Method,
 				req.RequestURI,
 				ctx.RealIP(),
-				resp.Status,
 				duration,
 				err,
 			)
